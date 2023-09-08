c5a010996399e25509b1a43d3011a8d310bac18b
philip oochi <oochiphilip@gmail.com>
2023-09-08 05:53:34 -0700

    c hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point 
+ *main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }

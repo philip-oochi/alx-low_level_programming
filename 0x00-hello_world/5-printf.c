889e0ba4d5a72d2e370e7309d2bf3de2d5dafe9b
philip oochi <oochiphilip@gmail.com>
2023-09-08 06:05:27 -0700

    c hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 /**
  * main - Entry point
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
 }
